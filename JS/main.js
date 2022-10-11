5d80be10798d80ee5fd612d06f5fae33b48470ea
showkattanvir <showkat2016tanvir@gmail.com>
2022-10-11 12:45:43 +0600

    third added

--- JS/main.js
+++ JS/main.js
@@ -13,6 +13,13 @@ function getInputValue(inputId) {
     return amountValue;
 }
 
+function updateTotalField(totalFieldId, amount) {
+    const totalElement = document.getElementById(totalFieldId);
+    const totalText = totalElement.innerText;
+    const previousTotal = parseFloat(totalText);
+    totalElement.innerText = previousTotal + amount;
+}
+
 
 
 document.getElementById('deposit-button').addEventListener('click', function () {
@@ -21,10 +28,12 @@ document.getElementById('deposit-button').addEventListener('click', function () 
      const depositAmount = parseFloat(depositAmountText); */
     const depositAmount = getInputValue('deposit-input');// এখানে depositeAmount টা লাগতেছে যেহেতু  এটি আরো কয়েকটা জায়গায় use হয়েছে।
 
-    const depositTotal = document.getElementById('deposit-total');
+    /* const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
     const previousDepositTotal = parseFloat(depositTotalText);
-    depositTotal.innerText = previousDepositTotal + depositAmount;
+    depositTotal.innerText = previousDepositTotal + depositAmount; */
+    updateTotalField('deposit-total', depositAmount)
+
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalaceTotal = parseFloat(balanceTotalText);
@@ -36,10 +45,12 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     const withdrawAmountText = withdrawInput.value;
     const withdrawAmount = parseFloat(withdrawAmountText); */
     const withdrawAmount = getInputValue("withdraw-input")
-    const withdrawTotal = document.getElementById('withdraw-total');
+    /* const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalText = withdrawTotal.innerText;
     const previousWithdrawTotal = parseFloat(withdrawTotalText)
-    withdrawTotal.innerText = withdrawAmount + previousWithdrawTotal;
+    withdrawTotal.innerText = withdrawAmount + previousWithdrawTotal; 
+    */
+    updateTotalField('withdraw-total', withdrawAmount)
     // বেলেন্স টোটাল এখানে লিখেছি
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
